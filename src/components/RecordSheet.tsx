74ba54515469e4ce23499b67cb64286d626a06fb
Alex Clayton <alexander.clayton.email@gmail.com>
2023-08-26 17:09:17 -0500

    formatting record sheet: heat data

--- src/components/RecordSheet.tsx
+++ src/components/RecordSheet.tsx
@@ -374,6 +374,47 @@ export const RecordSheet: React.FC = () => {
       </p>
       <div className="col-start-45 col-end-61 row-start-67 row-end-89"></div>
       <span className="col-start-45 col-end-61 row-start-68 row-end-89 border-2 border-black rounded-record"></span>
+      {/* Start Heat Data Content */}
+      <p className="col-start-46 col-end-47 row-start-69 row-end-71 text-2xs"> Heat Level*</p>
+      <p className="col-start-47 col-end-48 row-start-71 row-end-72 text-2xs text-center">30</p>
+      <p className="col-start-47 col-end-48 row-start-72 row-end-73 text-2xs text-center">28</p>
+      <p className="col-start-47 col-end-48 row-start-73 row-end-74 text-2xs text-center">26</p>
+      <p className="col-start-47 col-end-48 row-start-74 row-end-75 text-2xs text-center">25</p>
+      <p className="col-start-47 col-end-48 row-start-75 row-end-76 text-2xs text-center">24</p>
+      <p className="col-start-47 col-end-48 row-start-76 row-end-77 text-2xs text-center">23</p>
+      <p className="col-start-47 col-end-48 row-start-77 row-end-78 text-2xs text-center">22</p>
+      <p className="col-start-47 col-end-48 row-start-78 row-end-79 text-2xs text-center">20</p>
+      <p className="col-start-47 col-end-48 row-start-79 row-end-80 text-2xs text-center">19</p>
+      <p className="col-start-47 col-end-48 row-start-80 row-end-81 text-2xs text-center">18</p>
+      <p className="col-start-47 col-end-48 row-start-81 row-end-82 text-2xs text-center">17</p>
+      <p className="col-start-47 col-end-48 row-start-82 row-end-83 text-2xs text-center">15</p>
+      <p className="col-start-47 col-end-48 row-start-83 row-end-84 text-2xs text-center">14</p>
+      <p className="col-start-47 col-end-48 row-start-84 row-end-85 text-2xs text-center">13</p>
+      <p className="col-start-47 col-end-48 row-start-85 row-end-86 text-2xs text-center">10</p>
+      <p className="col-start-47 col-end-48 row-start-86 row-end-87 text-2xs text-center">8</p>
+      <p className="col-start-47 col-end-48 row-start-87 row-end-88 text-2xs text-center">5</p>
+      <p className="col-start-48 col-end-56 row-start-70 row-end-71 text-2xs text-center">Effects</p>
+      <p className="col-start-49 col-end-57 row-start-71 row-end-72 text-2xs">Shutdown</p>
+      <p className="col-start-49 col-end-57 row-start-72 row-end-73 text-2xs">Ammo Exp. avoid on 8+</p>
+      <p className="col-start-49 col-end-57 row-start-73 row-end-74 text-2xs">Sutdown, avoid on 10+</p>
+      <p className="col-start-49 col-end-57 row-start-74 row-end-75 text-2xs">-5 Movement Points</p>
+      <p className="col-start-49 col-end-57 row-start-75 row-end-76 text-2xs">+4 Modifier to Fire</p>
+      <p className="col-start-49 col-end-57 row-start-76 row-end-77 text-2xs">Ammo Exp. avoid on 6+</p>
+      <p className="col-start-49 col-end-57 row-start-77 row-end-78 text-2xs">Sutdown, avoid on 8+</p>
+      <p className="col-start-49 col-end-57 row-start-78 row-end-79 text-2xs">-4 Movement Points</p>
+      <p className="col-start-49 col-end-57 row-start-79 row-end-80 text-2xs">Ammo Exp. avoid on 4+</p>
+      <p className="col-start-49 col-end-57 row-start-80 row-end-81 text-2xs">Shutdown, avoid on 6+</p>
+      <p className="col-start-49 col-end-57 row-start-81 row-end-82 text-2xs">+3 Modifier to Fire</p>
+      <p className="col-start-49 col-end-57 row-start-82 row-end-83 text-2xs">-3 Movement Points</p>
+      <p className="col-start-49 col-end-57 row-start-83 row-end-84 text-2xs">Shutdown, avoid on 4+</p>
+      <p className="col-start-49 col-end-57 row-start-84 row-end-85 text-2xs">+2 Modifier to Fire</p>
+      <p className="col-start-49 col-end-57 row-start-85 row-end-86 text-2xs">-2 Movement Points</p>
+      <p className="col-start-49 col-end-57 row-start-86 row-end-87 text-2xs">+1 Modifier to fire</p>
+      <p className="col-start-49 col-end-57 row-start-87 row-end-88 text-2xs">-1 Movement Points</p>
+      <p className="col-start-53 col-end-61 row-start-69 row-end-70 text-xs">Double Heat Sinks:</p>
+      <div className="col-start-58 col-end-61 row-start-72 row-end-88"><BiCircle /><BiCircle /><BiCircle /><BiCircle /><BiCircle /><BiCircle /><BiCircle /><BiCircle /><BiCircle /><BiCircle /></div>
+
+      {/* End Heat Data Content */}
       <img
         src={HeatScale}
         alt=""
