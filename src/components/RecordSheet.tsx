29fec985b33dcb71db4acee1b97a15a522a46765
Alex Clayton <alexander.clayton.email@gmail.com>
2023-08-26 16:27:36 -0500

    formatting record sheet: critical hit table

--- src/components/RecordSheet.tsx
+++ src/components/RecordSheet.tsx
@@ -2,8 +2,10 @@
 import Logo from "../assets/BattletechBlackLogo.jpg";
 import Atlas from "../assets/AtlasCardImage.png";
 import ArmorDiagram from "../assets/ArmorDiagram.jpg";
+import DamageTransferDiagram from '../assets/DamageTransferDiagram.jpg'
 import InternalStructureDiagram from "../assets/InternalStructureDiagram.jpg";
 import HeatScale from "../assets/HeatScale.jpg";
+import { BiCircle } from 'react-icons/bi'
 
 export const RecordSheet: React.FC = () => {
   return (
@@ -118,20 +120,48 @@ export const RecordSheet: React.FC = () => {
         Piloting Skill:
       </p>
       <p className="col-start-42 col-end-44 row-start-14 row-end-15 text-2xs border-b border-black"></p>
-      <p className="col-start-30 col-end-35 row-start-16 row-end-17 text-2xs font-bold text-right">Hits Taken</p>
-      <p className="col-start-36 col-end-37 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">1</p>
-      <p className="col-start-37 col-end-38 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">2</p>
-      <p className="col-start-38 col-end-39 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">3</p>
-      <p className="col-start-39 col-end-40 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">4</p>
-      <p className="col-start-40 col-end-41 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">5</p>
-      <p className="col-start-41 col-end-43 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">6</p>
-      <p className="col-start-29 col-end-35 row-start-17 row-end-18 text-2xs font-bold text-right">Consciousness #</p>
-      <p className="col-start-36 col-end-37 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">3</p>
-      <p className="col-start-37 col-end-38 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">5</p>
-      <p className="col-start-38 col-end-39 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">7</p>
-      <p className="col-start-39 col-end-40 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">10</p>
-      <p className="col-start-40 col-end-41 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">11</p>
-      <p className="col-start-41 col-end-43 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">Dead</p>
+      <p className="col-start-30 col-end-35 row-start-16 row-end-17 text-2xs font-bold text-right">
+        Hits Taken
+      </p>
+      <p className="col-start-36 col-end-37 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">
+        1
+      </p>
+      <p className="col-start-37 col-end-38 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">
+        2
+      </p>
+      <p className="col-start-38 col-end-39 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">
+        3
+      </p>
+      <p className="col-start-39 col-end-40 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">
+        4
+      </p>
+      <p className="col-start-40 col-end-41 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">
+        5
+      </p>
+      <p className="col-start-41 col-end-43 row-start-16 row-end-17 border border-black text-2xs font-bold text-center pb-3">
+        6
+      </p>
+      <p className="col-start-29 col-end-35 row-start-17 row-end-18 text-2xs font-bold text-right">
+        Consciousness #
+      </p>
+      <p className="col-start-36 col-end-37 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">
+        3
+      </p>
+      <p className="col-start-37 col-end-38 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">
+        5
+      </p>
+      <p className="col-start-38 col-end-39 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">
+        7
+      </p>
+      <p className="col-start-39 col-end-40 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">
+        10
+      </p>
+      <p className="col-start-40 col-end-41 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">
+        11
+      </p>
+      <p className="col-start-41 col-end-43 row-start-17 row-end-18 border border-black text-2xs font-bold text-center pb-3">
+        Dead
+      </p>
       {/* End Warrior Data Content */}
       <img
         src={Atlas}
@@ -152,15 +182,184 @@ export const RecordSheet: React.FC = () => {
       <div className="col-start-6 col-end-44 row-start-42 row-end-89"></div>
       <span className="col-start-6 col-end-44 row-start-43 row-end-89 border-2 border-black rounded-record"></span>
       {/* Start Critical Hit Table Content */}
-      <p className="col-start-9 col-end-20 row-start-45 row-end-46 text-sm"><span className="font-bold">Left Arm</span> [CASE]</p>
-      <p className="col-start-8 col-end-20 row-start-47 row-end-48 text-xs font-bold">1. Shoulder</p>
-      <p className="col-start-8 col-end-20 row-start-48 row-end-49 text-xs font-bold">1. Shoulder</p>
-      <p className="col-start-8 col-end-20 row-start-49 row-end-50 text-xs font-bold">1. Shoulder</p>
-      <p className="col-start-8 col-end-20 row-start-50 row-end-51 text-xs font-bold">1. Shoulder</p>
-      <p className="col-start-8 col-end-20 row-start-51 row-end-52 text-xs font-bold">1. Shoulder</p>
-      <p className="col-start-8 col-end-20 row-start-52 row-end-53 text-xs font-bold">1. Shoulder</p>
-      <p className="col-start-22 col-end-30 row-start-45 row-end-46 text-sm font-bold">Head</p>
-      <p className="col-start-32 col-end-44 row-start-45 row-end-46 text-sm "><span className="font-bold">Right Arm</span> [CASE]</p>
+      <p className="col-start-10 col-end-20 row-start-45 row-end-46 text-sm">
+        <span className="font-bold">Left Arm</span> [CASE]
+      </p>
+      <p className="col-start-7 col-end-8 row-start-49 row-end-51 text-sm text-right font-bold">
+        1-3
+      </p>
+      <p className="col-start-9 col-end-20 row-start-47 row-end-48 text-2xs font-bold">
+        1. Shoulder
+      </p>
+      <p className="col-start-9 col-end-20 row-start-48 row-end-49 text-2xs font-bold">
+        2. Upper Arm Actuator
+      </p>
+      <p className="col-start-9 col-end-20 row-start-49 row-end-50 text-2xs font-bold">
+        3. Lower Arm Actuator
+      </p>
+      <p className="col-start-9 col-end-20 row-start-50 row-end-51 text-2xs font-bold">
+        4. Hand Actuator
+      </p>
+      <p className="col-start-9 col-end-20 row-start-51 row-end-52 text-2xs font-bold">
+        5. LRM 10
+      </p>
+      <p className="col-start-9 col-end-20 row-start-52 row-end-53 text-2xs font-bold">
+        6. Ammo [LRM 10] 12
+      </p>
+      <p className="col-start-7 col-end-8 row-start-56 row-end-58 text-sm text-right font-bold">
+        4-6
+      </p>
+      <p className="col-start-9 col-end-20 row-start-54 row-end-55 text-2xs">
+        <span className="font-bold">1.</span> Endo Steel
+      </p>
+      <p className="col-start-9 col-end-20 row-start-55 row-end-56 text-2xs">
+        <span className="font-bold">2.</span> Ferro-Fibrous
+      </p>
+      <p className="col-start-9 col-end-20 row-start-56 row-end-57 text-2xs">
+        <span className="font-bold">3.</span> Roll Again
+      </p>
+      <p className="col-start-9 col-end-20 row-start-57 row-end-58 text-2xs">
+        <span className="font-bold">4.</span> Roll Again
+      </p>
+      <p className="col-start-9 col-end-20 row-start-58 row-end-58 text-2xs">
+        <span className="font-bold">5.</span> Roll Again
+      </p>
+      <p className="col-start-9 col-end-20 row-start-59 row-end-60 text-2xs">
+        <span className="font-bold">6.</span> Roll Again
+      </p>
+      <p className="col-start-10 col-end-20 row-start-62 row-end-63 text-sm font-bold">Left Torso</p>
+      <p className="col-start-7 col-end-8 row-start-66 row-end-68 text-sm font-bold">1-3</p>
+      <p className="col-start-9 col-end-20 row-start-64 row-end-65 text-2xs font-bold">1. XL Fusion Engine</p>
+      <p className="col-start-9 col-end-20 row-start-65 row-end-66 text-2xs font-bold">2. XL Fusion Engine</p>
+      <p className="col-start-9 col-end-20 row-start-66 row-end-67 text-2xs font-bold">3. Double Heat Sink</p>
+      <p className="col-start-9 col-end-20 row-start-67 row-end-67 text-2xs font-bold">4. Double Heat Sink</p>
+      <p className="col-start-9 col-end-20 row-start-68 row-end-68 text-2xs font-bold">5. Double Heat Sink</p>
+      <p className="col-start-9 col-end-20 row-start-69 row-end-70 text-2xs font-bold">6. Double Heat Sink</p>
+      <p className="col-start-7 col-end-8 row-start-73 row-end-75 text-sm font-bold">4-6</p>
+      <p className="col-start-9 col-end-20 row-start-71 row-end-72 text-2xs"><span className="font-bold">1. Jump Jet</span></p>
+      <p className="col-start-9 col-end-20 row-start-72 row-end-73 text-2xs"><span className="font-bold">2. Jump Jet</span></p>
+      <p className="col-start-9 col-end-20 row-start-73 row-end-74 text-2xs"><span className="font-bold">3. Jump Jet</span></p>
+      <p className="col-start-9 col-end-20 row-start-74 row-end-75 text-2xs"><span className="font-bold">4.</span> Endo Steel</p>
+      <p className="col-start-9 col-end-20 row-start-75 row-end-76 text-2xs"><span className="font-bold">5.</span> Ferro-Fibrous</p>
+      <p className="col-start-9 col-end-20 row-start-76 row-end-77 text-2xs"><span className="font-bold">6.</span> Ferro-Fibrous</p>
+      <p className="col-start-10 col-end-20 row-start-79 row-end-80 text-sm font-bold">Left Leg</p>
+      <p className="col-start-9 col-end-20 row-start-81 row-end-82 text-2xs"><span className="font-bold">1. Hip</span></p>
+      <p className="col-start-9 col-end-20 row-start-82 row-end-83 text-2xs"><span className="font-bold">2. Upper Leg Actuator</span></p>
+      <p className="col-start-9 col-end-20 row-start-83 row-end-84 text-2xs"><span className="font-bold">3. Lower Leg Actuator</span></p>
+      <p className="col-start-9 col-end-20 row-start-84 row-end-85 text-2xs"><span className="font-bold">4. Foot Actuator</span></p>
+      <p className="col-start-9 col-end-20 row-start-85 row-end-86 text-2xs"><span className="font-bold">5.</span> Endo Steel</p>
+      <p className="col-start-9 col-end-20 row-start-86 row-end-87 text-2xs"><span className="font-bold">6.</span> Roll Again</p>
+
+      <p className="col-start-22 col-end-30 row-start-45 row-end-46 text-sm font-bold">
+        Head
+      </p>
+      <p className="col-start-21 col-end-30 row-start-47 row-end-48 text-2xs font-bold">
+        1. Life Support
+      </p>
+      <p className="col-start-21 col-end-30 row-start-48 row-end-49 text-2xs font-bold">
+        2. Sensors
+      </p>
+      <p className="col-start-21 col-end-30 row-start-49 row-end-50 text-2xs font-bold">
+        3. Cockpit
+      </p>
+      <p className="col-start-21 col-end-30 row-start-50 row-end-51 text-2xs font-bold">
+        4. Active Probe
+      </p>
+      <p className="col-start-21 col-end-30 row-start-51 row-end-52 text-2xs font-bold">
+        5. Sensors
+      </p>
+      <p className="col-start-21 col-end-30 row-start-52 row-end-53 text-2xs font-bold">
+        6. Life Support
+      </p>
+      <p className="col-start-22 col-end-30 row-start-55 row-end-56 text-sm font-bold">Center Torso</p>
+      <p className="col-start-19 col-end-21 row-start-59 row-end-61 text-sm font-bold">1-3</p>
+      <p className="col-start-21 col-end-30 row-start-57 row-end-58 text-2xs font-bold">1. XL Fusion Engine</p>
+      <p className="col-start-21 col-end-30 row-start-58 row-end-59 text-2xs font-bold">2. XL Fusion Engine</p>
+      <p className="col-start-21 col-end-30 row-start-59 row-end-60 text-2xs font-bold">3. XL Fusion Engine</p>
+      <p className="col-start-21 col-end-30 row-start-60 row-end-61 text-2xs font-bold">4. Gyro</p>
+      <p className="col-start-21 col-end-30 row-start-61 row-end-62 text-2xs font-bold">5. Gyro</p>
+      <p className="col-start-21 col-end-30 row-start-62 row-end-63 text-2xs font-bold">6. Gyro</p>
+      <p className="col-start-19 col-end-21 row-start-66 row-end-68 text-sm font-bold">4-6</p>
+      <p className="col-start-21 col-end-30 row-start-64 row-end-65 text-2xs"><span className="font-bold">1. Gyro</span></p>
+      <p className="col-start-21 col-end-30 row-start-65 row-end-66 text-2xs"><span className="font-bold">2. XL Fusion Engine</span></p>
+      <p className="col-start-21 col-end-30 row-start-66 row-end-67 text-2xs"><span className="font-bold">3. XL Fusion Engine</span></p>
+      <p className="col-start-21 col-end-30 row-start-67 row-end-68 text-2xs"><span className="font-bold">4. XL Fusion Engine</span></p>
+      <p className="col-start-21 col-end-30 row-start-68 row-end-69 text-2xs"><span className="font-bold">5.</span> Endo Steel</p>
+      <p className="col-start-21 col-end-30 row-start-69 row-end-70 text-2xs"><span className="font-bold">6.</span> Ferro-Fibrous</p>
+      <div className="col-start-20 col-end-31 row-start-71 row-end-77 border-2 border-black rounded-lg"></div>
+      <p className="col-start-20 col-end-26 row-start-72 row-end-73 text-xs font-bold text-right">Engine Hits</p>
+      <p className="col-start-20 col-end-26 row-start-73 row-end-74 text-xs font-bold text-right">Gyro Hits</p>
+      <p className="col-start-20 col-end-26 row-start-74 row-end-75 text-xs font-bold text-right">Sensor Hits</p>
+      <p className="col-start-20 col-end-26 row-start-75 row-end-76 text-xs font-bold text-right">Life Support</p>
+      <p className="col-start-27 col-end-31 row-start-72 row-end-73 text-xs font-bold text-right flex pt-[3px]"><BiCircle size={13}/><BiCircle size={13}/><BiCircle size={13}/></p>
+      <p className="col-start-27 col-end-31 row-start-73 row-end-74 text-xs font-bold text-right flex pt-[3px]"><BiCircle size={13}/><BiCircle size={13}/></p>
+      <p className="col-start-27 col-end-31 row-start-74 row-end-75 text-xs font-bold text-right flex pt-[3px]"><BiCircle size={13}/><BiCircle size={13}/></p>
+      <p className="col-start-27 col-end-31 row-start-75 row-end-76 text-xs font-bold text-right flex pt-[3px]"><BiCircle size={13}/></p>
+      <img src={DamageTransferDiagram} alt="" className="col-start-20 col-end-31 row-start-78 row-end-89"/>
+      <p className="col-start-34 col-end-44 row-start-45 row-end-46 text-sm"><span className="font-bold">Right Arm</span> [CASE]</p>
+      <p className="col-start-31 col-end-33 row-start-49 row-end-51 text-sm font-bold">
+        1-3
+      </p>
+      <p className="col-start-33 col-end-44 row-start-47 row-end-48 text-2xs font-bold">
+        1. Shoulder
+      </p>
+      <p className="col-start-33 col-end-44 row-start-48 row-end-49 text-2xs font-bold">
+        2. Upper Arm Actuator
+      </p>
+      <p className="col-start-33 col-end-44 row-start-49 row-end-50 text-2xs font-bold">
+        3. Lower Arm Actuator
+      </p>
+      <p className="col-start-33 col-end-44 row-start-50 row-end-51 text-2xs font-bold">
+        4. Hand Actuator
+      </p>
+      <p className="col-start-33 col-end-44 row-start-51 row-end-52 text-2xs font-bold">
+        5. Stream SRM 4
+      </p>
+      <p className="col-start-33 col-end-44 row-start-52 row-end-53 text-2xs font-bold">
+        6. Machine Gun
+      </p>
+      <p className="col-start-31 col-end-33 row-start-56 row-end-58 text-sm font-bold">
+        4-6
+      </p>
+      <p className="col-start-33 col-end-44 row-start-54 row-end-55 text-2xs">
+        <span className="font-bold">1. Machine Gun</span>
+      </p>
+      <p className="col-start-33 col-end-44 row-start-55 row-end-56 text-2xs">
+        <span className="font-bold">2. Ammo [Streak SRM 4] 25</span>
+      </p>
+      <p className="col-start-33 col-end-44 row-start-56 row-end-57 text-2xs">
+        <span className="font-bold">3. Ammo [Machine Gun] 100</span>
+      </p>
+      <p className="col-start-33 col-end-44 row-start-57 row-end-58 text-2xs">
+        <span className="font-bold">4.</span> Endo Steel
+      </p>
+      <p className="col-start-33 col-end-44 row-start-58 row-end-58 text-2xs">
+        <span className="font-bold">5.</span> Ferro-Fibrous
+      </p>
+      <p className="col-start-33 col-end-44 row-start-59 row-end-59 text-2xs">
+        <span className="font-bold">6.</span> Roll Again
+      </p>
+      <p className="col-start-34 col-end-44 row-start-62 row-end-63 text-sm font-bold">Right Torso</p>
+      <p className="col-start-31 col-end-33 row-start-66 row-end-68 text-sm font-bold">1-3</p>
+      <p className="col-start-33 col-end-44 row-start-64 row-end-65 text-2xs font-bold">1. XL Fusion Engine</p>
+      <p className="col-start-33 col-end-44 row-start-65 row-end-66 text-2xs font-bold">2. XL Fusion Engine</p>
+      <p className="col-start-33 col-end-44 row-start-66 row-end-67 text-2xs font-bold">3. Double Heat Sink</p>
+      <p className="col-start-33 col-end-44 row-start-67 row-end-68 text-2xs font-bold">4. Double Heat Sink</p>
+      <p className="col-start-33 col-end-44 row-start-68 row-end-69 text-2xs font-bold">5. Jump Jet</p>
+      <p className="col-start-33 col-end-44 row-start-69 row-end-70 text-2xs font-bold">6. Jump Jet</p>
+      <p className="col-start-33 col-end-44 row-start-71 row-end-72 text-2xs"><span className="font-bold">1. Jump Jet</span></p>
+      <p className="col-start-33 col-end-44 row-start-72 row-end-73 text-2xs"><span className="font-bold">2.</span> Endo Steel</p>
+      <p className="col-start-33 col-end-44 row-start-73 row-end-74 text-2xs"><span className="font-bold">3.</span> Ferro-Fibrous</p>
+      <p className="col-start-33 col-end-44 row-start-74 row-end-75 text-2xs"><span className="font-bold">4.</span> Ferro-Fibrous</p>
+      <p className="col-start-33 col-end-44 row-start-75 row-end-76 text-2xs"><span className="font-bold">5.</span> Roll Again</p>
+      <p className="col-start-33 col-end-44 row-start-76 row-end-77 text-2xs"><span className="font-bold">6.</span> Roll Again</p>
+      <p className="col-start-34 col-end-44 row-start-79 row-end-80 text-sm font-bold">Right Leg</p>
+      <p className="col-start-33 col-end-44 row-start-81 row-end-82 text-2xs"><span className="font-bold">1. Hip</span></p>
+      <p className="col-start-33 col-end-44 row-start-82 row-end-83 text-2xs"><span className="font-bold">2. Upper Leg Actuator</span></p>
+      <p className="col-start-33 col-end-44 row-start-83 row-end-84 text-2xs"><span className="font-bold">3. Lower Leg Actuator</span></p>
+      <p className="col-start-33 col-end-44 row-start-84 row-end-85 text-2xs"><span className="font-bold">4. Foot Actuator</span></p>
+      <p className="col-start-33 col-end-44 row-start-85 row-end-86 text-2xs"><span className="font-bold">5.</span> Endo Steel</p>
+      <p className="col-start-33 col-end-44 row-start-86 row-end-87 text-2xs"><span className="font-bold">6.</span> Roll Again</p>
       {/* End Critical Hit Table Content */}
       <p className="col-start-45 col-end-61 row-start-42 row-end-44 bg-black text-white text-sm text-center z-10 rounded-full pt-[2px]">
         INTERNAL STRUCTURE DIAGRAM
