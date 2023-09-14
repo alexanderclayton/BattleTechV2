e49653edff775a915ddb6cfa8af6b62f617dbaa0
Alex Clayton <alexander.clayton.email@gmail.com>
2023-09-14 12:51:21 -0500

    created image upload component, configured firebase storage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 //import//
-import { RecordSheet } from "../components/RecordSheet";
+import { AddImage } from "../components/AddImage";
 
 export const Home: React.FC = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-center">
       <div className="flex justify-start">
       </div>
-      <div className="flex justify-end">
-        <RecordSheet />
+      <div className="flex justify-center">
+        <AddImage />
       </div>
     </div>
   );
